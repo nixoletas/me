e224019d33def60e88819bdd770bf92a808bbbbd
nixoletas <nicholetas@gmail.com>
2024-10-14 23:22:36 -0400

    update data.js

--- src/utils/data.js
+++ src/utils/data.js
@@ -4,9 +4,9 @@ const projects = [
       title: 'GovBR Template',
       description:
         "This is a template of the real intranet project for the battalion i'm serving. " ,
-      codeLink: 'https://github.com/djamaile/ourmanga.community',
-      liveLink: 'https://ourmanga.community',
-      techstack: ['ReactJS', 'TypeScript', 'Golang'],
+      codeLink: 'https://github.com/nixoletas/gov-eb-intranet-template',
+      liveLink: 'https://nixoletas.github.io/gov-eb-intranet-template',
+      techstack: ['Angular', 'Strapi', 'Express', 'govbr'],
     },
     {
       img: 'img/docusaurus.png',
