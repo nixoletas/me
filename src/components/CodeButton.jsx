49c1258376d36b55dfc32546bbbd6a54a4ca73ec
Nicholas_Miyasato <nicholetas@gmail.com>
2024-10-20 01:15:59 -0400

    css adjustments

--- src/components/CodeButton.jsx
+++ src/components/CodeButton.jsx
@@ -5,23 +5,17 @@ import { faCode } from "@fortawesome/free-solid-svg-icons";
 export const CodeButton = ({codeLink}) => (
   <a
   style={{
-    marginRight: ".7rem",
-    padding: ".5rem .7rem",
-    textDecoration: "none",
+    textDecoration: "underline",
     color: "#007acc",
-    border: "1px solid #007acc",
-    borderRadius: "4px",
     transition: "background 0.3s, color 0.3s",
   }}
   href={codeLink}
   target="_blank"
   rel="noopener noreferrer"
   onMouseOver={(e) => {
-    e.currentTarget.style.background = "#007acc";
-    e.currentTarget.style.color = "#fff";
+    e.currentTarget.style.color = "#00003f";
   }}
   onMouseOut={(e) => {
-    e.currentTarget.style.background = "transparent";
     e.currentTarget.style.color = "#007acc";
   }}
 >
