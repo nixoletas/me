59cfdf66c5bd4f5c9bcaad8d08e1bff987d15ee3
Nicholas_Miyasato <nicholetas@gmail.com>
2024-10-20 00:49:57 -0400

    flexwrap to projects.mdx

--- src/pages/index.js
+++ src/pages/index.js
@@ -21,7 +21,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/experience">
+            to="/portfolio/experience">
             <Translate>About</Translate>
           </Link>
         </div>
