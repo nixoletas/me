6ef0eaa2571815de5692361aa0b27c426a6e4d24
nixoletas <nicholetas@gmail.com>
2024-12-05 08:30:55 -0400

    update config for deployment

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -13,15 +13,16 @@ const config = {
   favicon: 'img/logo_nixoletas.png',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://github.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/portfolio/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'nixoletas', // Usually your GitHub org/user name.
+  projectName: 'portfolio', // Usually your repo name.
+  deploymentBranch: 'gh-pages',
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
