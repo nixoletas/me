d188475d0d0ebf88dbb59cec59689aefd444261e
nixoletas <nicholetas@gmail.com>
2025-04-05 11:06:23 -0400

    refactor: enhance homepage layout and update button styles

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,49 +1,72 @@
-import clsx from 'clsx';
-import Link from '@docusaurus/Link';
-import Layout from '@theme/Layout';
-import HomepageFeatures from '@site/src/components/HomepageFeatures';
+import clsx from "clsx";
+import Link from "@docusaurus/Link";
+import Layout from "@theme/Layout";
+import HomepageFeatures from "@site/src/components/HomepageFeatures";
 
-import Heading from '@theme/Heading';
-import styles from './index.module.css';
-import Translate from '@docusaurus/Translate';
-import SocialIcons from '../components/SocialIcons';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faBook } from '@fortawesome/free-solid-svg-icons';
-import Grid from '../components/Grid';
-import Shadown from '../components/Shadown';
+import Heading from "@theme/Heading";
+import styles from "./index.module.css";
+import Translate from "@docusaurus/Translate";
+import SocialIcons from "../components/SocialIcons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBook, faFile, faEnvelope } from "@fortawesome/free-solid-svg-icons";
+import Grid from "../components/Grid";
+import Shadown from "../components/Shadown";
 
 function HomepageHeader() {
   return (
     <>
-    <header className={clsx('hero hero--primary', styles.heroBanner)}>
-          <Grid/>
-      <div className="container" style={{zIndex: 1}}>
-        <Heading as="h1" className="hero__title">
-          <Translate>Hi, I'm Nick.</Translate>
-        </Heading>
-        <img src="/me/img/github-profile.jpg" alt="GitHub" style={{width:"140px", height:"140px", marginLeft:"10px", borderRadius:"4rem"}} />
-        <p className="hero__subtitle"><Translate>I'm a Software Developer, Musician and Designer.</Translate></p>
-        <div className={styles.buttons}>
-          <Link
-            className="button button--secondary button--lg"
-            to="/portfolio/projects"
+      <header className={clsx("hero hero--primary", styles.heroBanner)}>
+        <Grid />
+        <div className="container" style={{ zIndex: 1 }}>
+          <Heading as="h1" className="hero__title">
+            <Translate>Hi, I'm Nick.</Translate>
+          </Heading>
+          <img
+            src="/me/img/github-profile.jpg"
+            alt="GitHub"
+            style={{
+              width: "140px",
+              height: "140px",
+              marginLeft: "10px",
+              borderRadius: "4rem",
+            }}
+          />
+          <p className="hero__subtitle">
+            <Translate>
+              I'm a Software Engineer, Musician and Designer.
+            </Translate>
+          </p>
+          <div
+            className={styles.buttons}
+            style={{ display: "flex", gap: "10px", flexDirection: "column" }}
+          >
+            <Link
+              className="button button--primary button--lg"
+              to="/portfolio/projects"
             >
-            <Translate>Projects </Translate><FontAwesomeIcon icon={faBook} width={15}/>
-          </Link>
+              <Translate>Projects </Translate>
+              <FontAwesomeIcon icon={faBook} width={15} />
+            </Link>
+            <Link
+              className="button button--primary button--lg"
+              href="/me/Nick_Miyasato.pdf"
+              target="_blank"
+            >
+              <Translate>Resume </Translate>
+              <FontAwesomeIcon icon={faEnvelope} width={15} />
+            </Link>
+          </div>
+          <SocialIcons />
         </div>
-        <SocialIcons />
-      </div>
-<Shadown />
-    </header>
+        <Shadown />
+      </header>
     </>
   );
 }
 
 export default function Home() {
   return (
-    <Layout
-    title={`Welcome`}
-    description="Nicholas Miyasato Portfolio & Blog">
+    <Layout title={`Welcome`} description="Nicholas Miyasato Portfolio & Blog">
       <HomepageHeader />
       <main>
         <HomepageFeatures />
