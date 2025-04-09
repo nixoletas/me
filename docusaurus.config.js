6f6109ebd036d21982bb6a3bf7e1009323d54104
nixoletas <nicholetas@gmail.com>
2025-04-09 19:25:53 -0400

    feat: add color mode configuration to docusaurus for improved user experience with dark mode settings

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -106,6 +106,11 @@ const config = {
         theme: prismThemes.github,
         darkTheme: prismThemes.dracula,
       },
+      colorMode: {
+        defaultMode: "dark",
+        disableSwitch: true,
+        respectPrefersColorScheme: false,
+      },
     }),
 };
 
