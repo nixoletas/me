4eb74311ee518b6a7314ae0ea9d1cb994f8443ad
nixoletas <nicholetas@gmail.com>
2025-04-09 18:57:58 -0400

    chore: update site URL in docusaurus configuration to point to new Netlify deployment

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -14,7 +14,7 @@ const config = {
   favicon: "img/logo_nixoletas.png",
 
   // Set the production url of your site here
-  url: "https://nixoletas.github.io",
+  url: "https://nickmiyasato.netlify.app",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: "/me/",
