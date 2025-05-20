63f097cea7d909f0a071d1b594e2097b32d5d0f7
nixoletas <nicholetas@gmail.com>
2025-05-20 10:25:31 -0300

    test changing textColor

--- src/components/CommentSection.jsx
+++ src/components/CommentSection.jsx
@@ -5,9 +5,11 @@ export const CommentSection = () => {
 
     useEffect(() => {
         const box = commentBox('5729242960625664-proj', {
+            textColor: '#fff',
+            subtextColor: '#fff'
         });
         return () => box();
-    });
+    }, []);
 
 
     return (
